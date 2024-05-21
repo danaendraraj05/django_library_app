ccc082cf3683e0c42c6b6154ba07e3eef9d74456
danaendraraj05 <danaendraraj@testpress.in>
2024-05-21 13:08:24 +0530

    Added Detailed View

--- locallibrary/locallibrary/catalog/views.py
+++ locallibrary/locallibrary/catalog/views.py
@@ -1,17 +1,41 @@
 from django.shortcuts import render
-from .models import Book, Author, BookInstance, Genre
+from django.views import generic
+from django.shortcuts import get_object_or_404
+from django.views.generic import DetailView
+from .models import Book, Author, BookInstance
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 10
+    context_object_name = 'book_list'
+    template_name = 'BookList.html'
+    ordering = ['title']
+
+class BookDetailView(generic.DetailView):
+    model = Book
+    template_name = 'book_detail.html'
+
+class AuthorView(generic.ListView):
+    model = Author
+    paginate_by = 10
+    context_object_name = 'authors_list'
+    template_name = 'author_list.html'
+    ordering = ['last_name'] 
+
+class AuthorDetailView(DetailView):
+    model = Author
+    template_name = 'author_detail.html'
+    context_object_name = 'author'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Add additional context data here if needed
+        return context
 
 def index(request):
-    """View function for home page of site."""
-
-    # Generate counts of some of the main objects
     num_books = Book.objects.all().count()
     num_instances = BookInstance.objects.all().count()
-
-    # Available books (status = 'a')
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
-
-    # The 'all()' is implied by default.
     num_authors = Author.objects.count()
 
     context = {
@@ -22,4 +46,3 @@ def index(request):
     }
 
     return render(request, 'index.html', context=context)
-
